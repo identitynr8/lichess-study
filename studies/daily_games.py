e68b5e94521064185aea6143528ca2842e07f050
max <mulogunov@gmail.com>
2020-03-14 12:51:16 +0100

    another month of data

--- studies/daily_games.py
+++ studies/daily_games.py
@@ -7,45 +7,72 @@ import numpy as np
 from utils.meta_info import get_meta_df
 
 
-def count_games() -> Tuple[dict, dict]:
+def count_games() -> Tuple[dict, dict, dict]:
     df = get_meta_df(usecols=['UTCDate', 'UTCTime'])
 
-    day_of_week_counters = {i: 0 for i in range(1, 8)}
+    day_of_week_counters = {i: {} for i in range(1, 8)}
     hourly_counters = {i: {str(h).rjust(2, '0'): 0 for h in range(0, 24)} for i in range(1, 8)}
+    daily_counters = {}
 
     c = 0
-
     for row in df.itertuples(index=False):
 
         y, m, d = row[0].split('.')
         day_of_week = date(int(y), int(m), int(d)).isoweekday()
 
-        day_of_week_counters[day_of_week] += 1
+        if row[0] not in day_of_week_counters[day_of_week]:
+            day_of_week_counters[day_of_week][row[0]] = 0
+        day_of_week_counters[day_of_week][row[0]] += 1
 
         h = row[1].split(':')[0]
         hourly_counters[day_of_week][h] += 1
 
+        if row[0] not in daily_counters:
+            daily_counters[row[0]] = 0
+        daily_counters[row[0]] += 1
+
         c += 1
-        if not c % 1000:
+        if not c % 100000:
             print(c)
-    return day_of_week_counters, hourly_counters
+
+    for h, data in hourly_counters.items():
+        hourly_counters[h] = {k: v / len(day_of_week_counters[h]) for k, v in data.items()}
+
+    for day, data in day_of_week_counters.items():
+        day_of_week_counters[day] = sum(data.values()) / len(data)
+
+    return daily_counters, day_of_week_counters, hourly_counters
 
 
-def plot_daily_games(day_of_week_counters: dict):
+def plot_day_of_week_games(day_of_week_counters: dict):
     fig, ax = plt.subplots()
 
     weekdays = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
     y_pos = np.arange(len(weekdays))
     num_games = list(day_of_week_counters.values())
-    num_games_pct = [n / sum(num_games) * 100 for n in num_games]
 
-    ax.barh(y_pos, num_games_pct, align='center')
+    ax.barh(y_pos, num_games, align='center')
     ax.set_yticks(y_pos)
     ax.set_yticklabels(weekdays)
     ax.invert_yaxis()
-    ax.set_xlabel('% of all games played')
+    ax.set_xlabel('average games played')
 
     plt.savefig('../images/day_of_week_games.png')
+
+
+def plot_daily_games(daily_counters: dict):
+    fig, ax = plt.subplots()
+
+    x = list(daily_counters.keys())
+    ax.plot(x, list(daily_counters.values()))
+    for i in range(1, len(x)-1):
+        x[i] = ''
+    ax.set_xticklabels(x)
+
+    plt.grid(color='0.95')
+    plt.ylim(bottom=0, top=1.7*1000000)
+    plt.title('daily games')
+    plt.savefig('../images/daily_games.png')
 
 
 def plot_games_vs_utc_hours(hourly_counters: dict):
@@ -55,18 +82,20 @@ def plot_games_vs_utc_hours(hourly_counters: dict):
     for dow, data in hourly_counters.items():
         ax.plot(list(data.keys()), list(data.values()), label=weekdays[dow-1])
 
-    ax.set_yticklabels([])
     plt.grid(color='0.95')
     plt.legend()
-    plt.title('num. games vs UTC hours')
+    plt.title('average games vs UTC hours')
     plt.savefig('../images/num_games_vs_utc_hours.png')
 
 
 if __name__ == '__main__':
-    day_of_week_counters, hourly_counters = count_games()
-    
+    daily_counters, day_of_week_counters, hourly_counters = count_games()
+
     # plot daily games
-    plot_daily_games(day_of_week_counters)
+    plot_daily_games(daily_counters)
+
+    # plot day of week games
+    plot_day_of_week_games(day_of_week_counters)
 
     # plot games vs utc hours
     plot_games_vs_utc_hours(hourly_counters)
--- utils/meta_info.py
+++ utils/meta_info.py
@@ -25,28 +25,32 @@ if __name__ == '__main__':
     header_elements = ('Event', 'Date', 'White', 'Black', 'Result', 'UTCDate', 'UTCTime', 'WhiteElo', 'BlackElo',
                        'ECO', 'Opening', 'TimeControl', 'Termination')
 
+    games_files = ['lichess_db_standard_rated_2020-01.pgn', 'lichess_db_standard_rated_2020-02.pgn']
+
     with open(meta_file_path, 'w', newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter='|')
         writer.writerow(header_elements)
 
-        with open('../games/lichess_db_standard_rated_2020-02.pgn') as games_file:
-            d = {h: '' for h in header_elements}
-            headers_found = False
-            rows_counter = 0
+        for f in games_files:
+            with open('../games/{0}'.format(f)) as games_file:
+                d = {h: '' for h in header_elements}
+                headers_found = False
+                rows_counter = 0
 
-            for line in games_file:
-                if line.startswith('['):
-                    txt = line[1:-3]
-                    for h in header_elements:
-                        if txt.startswith(h+' '):
-                            txt = txt.replace(h + ' "', '')
-                            d[h] = txt
-                            headers_found = True
-                            break
+                for line in games_file:
+                    if line.startswith('['):
+                        txt = line[1:-3]
+                        for h in header_elements:
+                            if txt.startswith(h+' '):
+                                txt = txt.replace(h + ' "', '')
+                                d[h] = txt
+                                headers_found = True
+                                break
 
-                if line == '\n' and headers_found:
-                    writer.writerow([d[el] for el in header_elements])
-                    d = {h: '' for h in header_elements}
-                    headers_found = False
-                    rows_counter += 1
-                    print(rows_counter)
+                    if line == '\n' and headers_found:
+                        writer.writerow([d[el] for el in header_elements])
+                        d = {h: '' for h in header_elements}
+                        headers_found = False
+                        rows_counter += 1
+                        if not rows_counter % 1000:
+                            print(rows_counter)
